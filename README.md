6f58d7fd59c61e4e961eeda7cc93937232613ff2
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2024-06-02 17:22:52 -0600

    1st commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# GATEWAY SERVICE
+## Description
+Service for entrypoint of microservices
+
+## Dependencies
+- Spring cloud gateway
+- Spring cloud Netflix Eureka client
+- Spring cloud actuator
+
---
+++ src/main/java/io/github/saulmmbp/gatewayService/MicroservicesGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.saulmmbp.gatewayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesGatewayServiceApplication.class, args);
+	}
+
+}
